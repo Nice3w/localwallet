ba89b6f6747b39c07f180db0200603ffce1b9e8b
Nice3w <129052474+Nice3w@users.noreply.github.com>
2023-06-02 11:22:11 +0000

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,28 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, coinbaseWallet, localWallet, metamaskWallet, paperWallet, safeWallet, walletConnect } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider 
+      activeChain={activeChain}
+      supportedWallets={[
+        metamaskWallet(),
+        // coinbaseWallet(),
+        // walletConnect(),
+        // safeWallet(),
+        paperWallet({
+          clientId: "9954b111-1391-4aa7-ab8a-b3a0318b6a1a",
+        }),
+        localWallet()
+      ]}
+    
+    >
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,11 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
+          Benvingut al mon Web3!
         </h1>
 
         <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
+          Entra a formar part de la comunitat més gran descentralitzada!
         </p>
 
         <div className={styles.connect}>
@@ -21,31 +19,28 @@ const Home: NextPage = () => {
         </div>
 
         <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
+          <div  className={styles.card}>
+            <h2>Comunitat &rarr;</h2>
             <p>
               Guides, references and resources that will help you build with
               thirdweb.
             </p>
-          </a>
+          </div>
 
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
+          <div className={styles.card}>
+            <h2>Promocions &rarr;</h2>
             <p>
               Deploy, configure and manage your smart contracts from the
               dashboard.
             </p>
-          </a>
+          </div>
 
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
+          <div className={styles.card}>
+            <h2>Continguts &rarr;</h2>
             <p>
               Discover and clone template projects showcasing thirdweb features.
             </p>
-          </a>
+          </div>
         </div>
       </main>
     </div>
